7b1e868c5abb8817c8fd2b920ca7dcc7e142e69e
AbhishekAtoriya <151026630+AbhishekAtoriya@users.noreply.github.com>
2024-08-11 23:54:09 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -19,7 +19,7 @@ function Signup() {
     };
 
     axios
-      .post("http://localhost:4001/user/signup", userInfo)
+      .post("https://bookstoreapp-backend-9oa7.onrender.com", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
