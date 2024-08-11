6b0dc3acae2ad62e94488453b9786dd9a977c7e4
AbhishekAtoriya <151026630+AbhishekAtoriya@users.noreply.github.com>
2024-08-12 00:15:49 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
     };
 
     await axios
-      .post("https://bookstoreapp-backend-9oa7.onrender.com", userInfo)
+      .post("http://localhost:4001/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
